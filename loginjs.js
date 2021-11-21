ac7328bcd748bde863b09b897d7ed791766954d4
thewachdog <sit20it059@sairamtap.edu.in>
2021-11-21 15:36:04 +0000

    AddedLoginPage

--- loginjs.js
+++ loginjs.js
@@ -1,11 +1,6 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const toggleForm = () => {
+	const container = document.querySelector('.containers');
+	container.classList.toggle('active');
+  };
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+  
