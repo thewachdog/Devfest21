d4b6b6018edb5f49a8a4acc749c01fd505a2b80f
thewachdog <sit20it059@sairamtap.edu.in>
2021-11-22 09:24:16 +0000

    Commited

---
+++ loginjs.js
@@ -0,0 +1,9 @@
+const toggleForm = () => {
+	const container = document.querySelector('.containers');
+	container.classList.toggle('active');
+  };
+
+  
+var usernam = document.getElementById('username').value;
+
+console.log(usernam);
---
+++ reactui/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Resturants from "./orders/Components/Resturants";
+
+function App() {
+  return (
+    <div className="App">
+      <Resturants />
+    </div>
+  );
+}
+
+export default App;
